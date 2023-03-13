6241197e5d3dfcaed3f34a8ce359b116d6cd72b2
Amina184 <94177629+Amina184@users.noreply.github.com>
2023-03-13 21:26:36 +0000

    a

---
+++ app.py
@@ -0,0 +1,11 @@
+#fonction d'addition
+def summ(a, b):
+    return a + b
+
+#fonction de soustraction
+def minus(a, b):
+    return a - b
+
+#fonction de multiplication
+def multiply(a, b):
+    return a * b
